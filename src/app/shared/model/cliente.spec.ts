641b84cb8e3bedbb28fbbb3f351a25a84d72c144
amanda.p.silva <amanda.p.silva@accenture.com>
2020-02-19 19:46:22 -0300

    Testes unitários Cliente
    Signed-off-by: amanda.p.silva <amanda.p.silva@accenture.com>

--- src/app/shared/model/cliente.spec.ts
+++ src/app/shared/model/cliente.spec.ts
@@ -3,26 +3,34 @@ import { Estado } from './estado';
 import { Cidade } from './cidade';
 
 fdescribe('Cliente', () => {
-
+  let cliente: Cliente;
   // tslint:disable-next-line: prefer-const
-  let component: Cliente;
   let estado: Estado;
+  // tslint:disable-next-line: prefer-const
   let cidade: Cidade;
+
+  beforeEach(() => {
+    cliente = new Cliente(
+      'Amanda',
+      'Brasil',
+      31,
+      new Estado('São Paulo', 'SP'),
+      new Cidade('São Paulo', new Estado('São Paulo', 'SP')));
+  });
 
   it('should create an instance', () => {
     expect(new Cliente('Amanda', 'Brasil', 31, estado, cidade)).toBeTruthy();
   });
 
-  // it('should CriarNovoCliente', () => {
-  //   const name = 'Amanda';
-  //   component.setName(name);
-  //   expect(component.setName(name)).toBeUndefined();
-  //   // expect(component.getName()).toEqual(name);
-  // });
+  it('should CriarNovoCliente', () => {
+    const name = 'Amanda';
+    cliente.setName(name);
+    expect(cliente.setName(name)).toBeUndefined();
+  });
 
   it('CriarNovoCliente2', () => {
-    component.setName('Amanda');
-    expect(component.getName()).toEqual(name);
+    cliente.setName('Amanda');
+    expect(cliente.getName()).toEqual('Amanda');
   });
 
 });
