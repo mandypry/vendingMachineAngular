8b0213fbe716908b6db2180e2de885078ecc569b
amanda.p.silva <amanda.p.silva@accenture.com>
2020-02-24 00:02:09 -0300

    teste unitario Produto
    Signed-off-by: amanda.p.silva <amanda.p.silva@accenture.com>

--- src/app/shared/model/produto.spec.ts
+++ src/app/shared/model/produto.spec.ts
@@ -5,15 +5,41 @@ import { SubCategoria } from './subCategoria';
 describe('Produto', () => {
 
   // tslint:disable-next-line: prefer-const
-  let component: Produto;
+  let produto: Produto;
   // tslint:disable-next-line: prefer-const
   let categoria: Categoria;
   // tslint:disable-next-line: prefer-const
   let subCategoria: SubCategoria;
 
-  it('should create an instance', () => {
-    expect(new Produto('Suco de Frutas Vermelhas', 3.10, categoria, subCategoria)).toBeTruthy();
+  beforeEach(() => {
+    categoria = new Categoria('Alimenticio');
+    subCategoria = new SubCategoria('Liquido', categoria);
+    produto = new Produto(
+      'Suco de Frutas Vermelhas', 
+      10.40,
+      categoria,
+      subCategoria
+      );
   });
 
-  
+  it('should nameProduto', () => {
+    produto.setName('Suco de Frutas Vermelhas');
+    expect(produto.getName()).toEqual('Suco de Frutas Vermelhas');
+  });
+
+  it('should priceProduto', () => {
+    produto.setPrice(10.40);
+    expect(produto.getPrice()).toEqual(10.40);
+  });
+
+  it('should categoriaProduto', () => {
+    produto.setCategoria(categoria);
+    expect(produto.getCategoria()).toEqual(categoria);
+  });
+
+  it('should subCategoriaProduto', () => {
+    produto.setSubCategoria(subCategoria);
+    expect(produto.getSubCategoria()).toEqual(subCategoria);
+  });
+
 });
